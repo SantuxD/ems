a63dbf776de229105a3b689f19fc6f80c3b9c13e
Santu Mondal <mondalsantu104@gmail.com>
2025-07-21 22:52:19 +0530

    Add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import Login from './Component/Auth/Login'
 import EmployeeDashboard from './Component/Dashboard/EmployeeDashboard'
 import AdminDashboard from './Component/Dashboard/AdminDashboard'
+import { setLocalStorage } from './utils/Localstorage'
 
-function App() {
-  
+function App() { 
+ useEffect(() => {
+   setLocalStorage()
+ },)  
 
   return (
     <>
---
+++ src/utils/Localstorage.jsx
@@ -0,0 +1,44 @@
+const employee = [
+  {
+    id: 1,
+    email: "employee1@example.com",
+    password: "123",
+  },
+  {
+    id: 2,
+    email: "employee2@example.com",
+    password: "123",
+  },
+  {
+    id: 3,
+    email: "employee3@example.com",
+    password: "123",
+  },
+  {
+    id: 4,
+    email: "employee4@example.com",
+    password: "123",
+  },
+  {
+    id: 5,
+    email: "employee5@example.com",
+    password: "123",
+  },
+];
+
+const admin = [
+  {
+    id: 1,
+    email: "admin@example.com",
+    password: "123",
+  },
+];
+export const setLocalStorage = (key, value) => {
+  localStorage.setItem(employee, JSON.stringify(employee));
+  localStorage.setItem(admin, JSON.stringify(admin));
+};
+export const getLocalStorage = (key) => {
+  const employee = JSON.parse(localStorage.getItem("employee")) ;
+   const admin = JSON.parse(localStorage.getItem('admin'));
+  console.log(employee , admin);
+};
